c6bf69f198abbf3a093c8c98cc6efd84b0c05436
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-06 19:07:15 -0600

    Adjusted broken cron job

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
 
 const server = require("./server");
-
 const port = process.env.PORT || 4000;
+const cron = require("node-cron");
+
+const sessionDataParse = require("./models/sessionsDataParser");
+
+cron.schedule("*/10 * * * *", () => {
+  sessionDataParse();
+});
+
 server.listen(port, () => {
   console.log(`\n** Running on port ${port} **\n`);
 });
