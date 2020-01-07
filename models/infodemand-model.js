540caa1e1d375eedeabb9f1a704818729334ff8c
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-06 18:37:01 -0600

    Testing a table truncation, and reinsertation every 15 minutes

--- models/infodemand-model.js
+++ models/infodemand-model.js
@@ -1,9 +1,18 @@
 const db = require("../data/dbConfig");
 
 const batchInsert = (rows) => {
-  return db.batchInsert('information_demand', rows, 1000);
+  return db.batchInsert('info_demand_test', rows, 1000).then(() => {
+    return db('info_demand_test').count('*').then(res => console.log("ADDED", res));
+  });
+}
+
+const testTruncate = () => {
+  return db('info_demand_test').truncate().then(() => {
+    return db('info_demand_test').count('*').then(res => console.log("DELETED", res))
+  });
 }
 
 module.exports = {
-  batchInsert
+  batchInsert,
+  testTruncate
 };
